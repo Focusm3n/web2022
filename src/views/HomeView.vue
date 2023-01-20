20026ba4753f8a021f7969200541c80c1ac46632
Ilya <ilya.k007123@gmail.com>
2023-01-20 16:45:21 +0300

    fast commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,105 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="content">
+        <div class="container pt-50">
+            <div class="row">
+                <div class="home-categories__item">
+                    <a href="#" class="home-categories__info p_rel">
+                        <span class="sky"></span>
+                        <span class="home-categories__img-wrap">
+                                <img src="images/categories/1.png" alt="Кирпичи" class="home-categories__img">
+                            </span>
+                        <span class="home-categories__name">
+                                Кирпичи
+                            </span>
+                    </a>
+                </div>
+                <div class="home-categories__item">
+                    <a href="#" class="home-categories__info p_rel">
+                        <span class="sky"></span>
+                        <span class="home-categories__img-wrap">
+                                <img src="images/categories/2.png" alt="Кирпичи" class="home-categories__img">
+                            </span>
+                        <span class="home-categories__name">
+                                Блоки
+                            </span>
+                    </a>
+                </div>
+                <div class="home-categories__item">
+                    <a href="#" class="home-categories__info p_rel">
+                        <span class="sky"></span>
+                        <span class="home-categories__img-wrap">
+                                <img src="images/categories/0.png" alt="Кирпичи" class="home-categories__img">
+                            </span>
+                        <span class="home-categories__name">
+                                Тротуарная плитка
+                            </span>
+                    </a>
+                </div>
+                <div class="home-categories__item">
+                    <a href="#" class="home-categories__info p_rel">
+                        <span class="sky"></span>
+                        <span class="home-categories__img-wrap">
+                                <img src="images/categories/3.png" alt="Кирпичи" class="home-categories__img">
+                            </span>
+                        <span class="home-categories__name">
+                                Природный камень
+                            </span>
+                    </a>
+                </div>
+                <div class="home-categories__item">
+                    <a href="#" class="home-categories__info p_rel">
+                        <span class="sky"></span>
+                        <span class="home-categories__img-wrap">
+                                <img src="images/categories/1.png" alt="Кирпичи" class="home-categories__img">
+                            </span>
+                        <span class="home-categories__name">
+                                Кирпичи
+                            </span>
+                    </a>
+                </div>
+                <div class="home-categories__item">
+                    <a href="#" class="home-categories__info p_rel">
+                        <span class="sky"></span>
+                        <span class="home-categories__img-wrap">
+                                <img src="images/categories/2.png" alt="Кирпичи" class="home-categories__img">
+                            </span>
+                        <span class="home-categories__name">
+                                Блоки
+                            </span>
+                    </a>
+                </div>
+                <div class="home-categories__item">
+                    <a href="#" class="home-categories__info p_rel">
+                        <span class="sky"></span>
+                        <span class="home-categories__img-wrap">
+                                <img src="images/categories/0.png" alt="Кирпичи" class="home-categories__img">
+                            </span>
+                        <span class="home-categories__name">
+                                Тротуарная плитка
+                            </span>
+                    </a>
+                </div>
+                <div class="home-categories__item">
+                    <a href="#" class="home-categories__info p_rel">
+                        <span class="sky"></span>
+                        <span class="home-categories__img-wrap">
+                                <img src="images/categories/3.png" alt="Кирпичи" class="home-categories__img">
+                            </span>
+                        <span class="home-categories__name">
+                                Природный камень
+                            </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    name: 'HomeView',
+    components: {
+    }
 }
 </script>
